54b05b688b6a5f5dd0bc17b71de8329e64fe7694
rachiderrami <errami.rachid@outlook.com>
2023-01-31 21:30:01 -0500

    adding vercel

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-python3.10 -m install -r requirements.txt 
-python3.10 manage.py collectstatic --noinput --clear
+python3 -m install -r requirements.txt 
+python3 manage.py collectstatic --noinput --clear
 
 echo " BUILD END"
